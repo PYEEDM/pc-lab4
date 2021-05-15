52f2543516884454778728284cba6d8d33f8d723
PYEEDM <pye.edm@gmail.com>
2021-05-15 21:57:40 +0200

    Fix typo

--- src/relax-unoptimized-mpi.c
+++ src/relax-unoptimized-mpi.c
@@ -70,7 +70,7 @@ void relax( double *in, double *out, size_t n, int start_index, int length)
    last_i = last_i < n-1 ? last_i : n-1;
    for( i=init_i; i<last_i; i++) {
       init_j = i == init_i ? init_j : 1;
-      last_j = i < last_i - 1 ? last_j : (start_index+lengths)%n;
+      last_j = i < last_i - 1 ? last_j : (start_index+length)%n;
       for( j=init_j; j<last_j; j++) {
          out[i*n+j] = 0.25*in[(i-1)*n+j]      // upper neighbour
                       + 0.25*in[i*n+j]        // center
