656d6fd167cb0e8fc09dd01aeaff6583fca25752
Seraph Jin <zjin@cn13.science.ru.nl>
2021-05-25 20:39:49 +0200

    new relaex-script

--- src/relax-script.sh
+++ src/relax-script.sh
@@ -1,30 +1,64 @@
 #!/usr/bin/env bash
 
 #SBATCH -p csedu
+
 echo "compiling..."
 
-gcc relax-sequential-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-seq-vec.o
-gcc -fopenmp relax-omp-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-omp-vec.o
-mpicc -DMPI relax-mpi-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-mpi-vec.o 
+make
+
 echo "!!!!!!!size of matrix!!!!!!!!"
 
 n=256
-while (( $n <= 35000 ))
+m=1
+while (( $n <= 350 ))
 do
 	echo "matrix $n * $n."
-    ./relax-seq-vec.o $n 1
-	./relax-omp-vec.o $n 1 1
-	mpirun -N 1 relax-mpi-vec.o $n 1 
-	n=$(( 2*n ))	
+	while (($m <= 2))
+	do
+		echo "cores: $m"
+		for i in `seq 1 5`
+		do
+			echo "measurement: $i"
+			if ((m==1))
+			then
+				./relax-seq.o $n 1
+				./relax-seq-vec.o $n 1
+			fi
+			./relax-omp.o $n 1 $m
+			./relax-omp-vec.o $n 1 $m
+			mpirun -N $m ./relax-mpi.o $n 1 
+			mpirun -N $m ./relax-mpi-vec.o $n 1 
+				
+		done
+		m=$((2*m))
+	done	
+	n=$(( 2*n ))
 done
 
 echo "!!!!!!!iteration number!!!!!!!!"
 itr=100
-while (( $itr <= 1000 ))
-do
+m=1
+while (( $itr <= 100 ))
+do	
 	echo "iterations: $itr"
-    ./relax-seq-vec.o 1024 $itr
-	./relax-omp-vec.o 1024 $itr 1
-	mpirun -N 1 relax-mpi-vec.o 1024 $itr 
+	while (($m <= 2))
+	do
+		echo "number of cores: $m"
+		for i in `seq 1 5`
+		do
+		echo "measurement: $i"
+		if ((m==1))
+		then
+			./relax-seq.o 2048 $itr
+			./relax-seq-vec.o 2048 $itr
+		fi
+		./relax-omp.o 2048 $itr $m
+		./relax-omp-vec.o 2048 $itr $m
+		mpirun -N $m relax-mpi.o 2048 $itr
+		mpirun -N $m relax-mpi-vec.o 2048 $itr 
+		
+		done
+		m=$((2*m))
+	done
 	itr=$(( 100+itr ))	
 done
