6605df1fc17d691d6766dfc923e82696b94b0c66
Seraph Jin <zjin@cn13.science.ru.nl>
2021-05-25 18:04:04 +0200

    new version

--- src/relax-script.sh
+++ src/relax-script.sh
@@ -3,29 +3,28 @@
 #SBATCH -p csedu
 echo "compiling..."
 
-gcc relax-optimized-sequential.c job-time.c -O3 -march=native -fopt-info-vec -o relax-seq-op.o
-gcc -fopenmp relax-optimized-omp.c job-time.c -O3 -march=native -fopt-info-vec -o relax-omp-op.o
-mpicc -DMPI relax-optimized-mpi.c job-time.c -O3 -march=native -fopt-info-vec -o relax-mpi-op.o 
-
+gcc relax-sequential-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-seq-vec.o
+gcc -fopenmp relax-omp-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-omp-vec.o
+mpicc -DMPI relax-mpi-vec.c job-time.c -O3 -march=native -fopt-info-vec -o relax-mpi-vec.o 
 echo "!!!!!!!size of matrix!!!!!!!!"
 
-n=100
+n=256
 while (( $n <= 35000 ))
 do
 	echo "matrix $n * $n."
-    ./relax-seq-op.o $n 1
-	./relax-omp-op.o $n 1 1
-	mpirun relax-mpi-op.o $n 1 
+    ./relax-seq-vec.o $n 1
+	./relax-omp-vec.o $n 1 1
+	mpirun -N 1 relax-mpi-vec.o $n 1 
 	n=$(( 2*n ))	
 done
 
 echo "!!!!!!!iteration number!!!!!!!!"
 itr=100
-while (( $itr <= 35000 ))
+while (( $itr <= 1000 ))
 do
 	echo "iterations: $itr"
-    ./relax-seq-op.o 64 $itr
-	./relax-omp-op.o 64 $itr 1
-	mpirun relax-mpi-op.o 64 $utr 
-	itr=$(( 2*itr ))	
+    ./relax-seq-vec.o 1024 $itr
+	./relax-omp-vec.o 1024 $itr 1
+	mpirun -N 1 relax-mpi-vec.o 1024 $itr 
+	itr=$(( 100+itr ))	
 done
