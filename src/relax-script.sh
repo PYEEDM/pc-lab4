c0f7432e2ff4722def7f48d122c83d599184e5e3
Sumi <56109874+Seraaphonano@users.noreply.github.com>
2021-05-25 15:38:00 +0200

    Update relax-script.sh
    new sbatcg script

--- src/relax-script.sh
+++ src/relax-script.sh
@@ -1,47 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#compile files
-make
+#SBATCH -p csedu
+echo "compiling..."
 
-#!/bin/bash
-n=1
-while (( $n <= 350 ))
+gcc relax-optimized-sequential.c job-time.c -O3 -march=native -fopt-info-vec -o relax-seq-op.o
+gcc -fopenmp relax-optimized-omp.c job-time.c -O3 -march=native -fopt-info-vec -o relax-omp-op.o
+mpicc -DMPI relax-optimized-mpi.c job-time.c -O3 -march=native -fopt-info-vec -o relax-mpi-op.o 
+
+
+n=100
+while (( $n <= 35000 ))
 do
 	echo "matrix $n * $n."
-    ./relax-seq.o $n 1
+    ./relax-seq-op.o $n 1
+	./relax-omp-op.o $n 1 1
+	relax-mpi-op.o $n 1 
 	n=$(( 2*n ))	
 done
 
-# n=1
-# ./a.out 512 $n
-# n=100
-# while (( $n <= 1000 ))
-# do
-#     ./relax-seq.o 512 $n 
-# 	n=$(( 100+n ))	
-# done
 
-
-# for i in $(seq 1 10 20) #size
-# do
-#     for j in $(seq 0 10 20) #iter nr
-#     do 
-#         for t in $(seq 1 2)#threads nr
-#         do
-#         echo "size: $i, iter: $j  , thread= $t "
-#         ./original.o $i $j
-#         ./openmp.o $i $j $t
-#         mpirun -N $t ./mpi.o $i $j
-        
-#         done
-#     done
-# done
-
-
-
-
-# SIZE="${1:-10}"
-
-# ITER_NR="${2:-100}"
-
-# THREAD_NR="${3:-1}"
+itr=100
+while (( $itr <= 35000 ))
+do
+	echo "iterations: $itr"
+    ./relax-seq-op.o 64 $itr
+	./relax-omp-op.o 64 $itr 1
+	relax-mpi-op.o 64 $utr 
+	itr=$(( 2*itr ))	
+done
