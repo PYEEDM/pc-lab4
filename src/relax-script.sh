56fbdb4c71bc33713bcc7f86e53ab6f488ab30b7
PYEEDM <pye.edm@gmail.com>
2021-05-25 22:28:32 +0200

    Fix some bugs in the script

--- src/relax-script.sh
+++ src/relax-script.sh
@@ -2,63 +2,61 @@
 
 #SBATCH -p csedu
 #SBATCH --cpus-per-task 32
-echo "compiling..."
 
+echo "Compiling..."
 make
 
-echo "!!!!!!!size of matrix!!!!!!!!"
-
+echo "Running experiment for variable matrix size..."
 n=256
-m=1
-while (( $n <= 35000 ))
+c=1
+while (( $n <= 32768 ))
 do
-	echo "matrix $n * $n."
-	while (($m <= 2))
+	echo "Matrix size: $n * $n."
+	while (( $c <= 32 ))
 	do
-		echo "cores: $m"
+		echo "Number of cores: $c"
 		for i in `seq 1 5`
 		do
-			echo "measurement: $i"
-			if ((m==1))
+			echo "Measurement: $i"
+			if (( c==1 ))
 			then
 				./relax-seq.o $n 1
 				./relax-seq-vec.o $n 1
 			fi
-			./relax-omp.o $n 1 $m
-			./relax-omp-vec.o $n 1 $m
-			mpirun -N $m ./relax-mpi.o $n 1 
-			mpirun -N $m ./relax-mpi-vec.o $n 1 
+			./relax-omp.o $n 1 $c
+			./relax-omp-vec.o $n 1 $c
+			mpirun -n $c ./relax-mpi.o $n 1 
+			mpirun -n $c ./relax-mpi-vec.o $n 1 
 				
 		done
-		m=$((2*m))
+		m=$(( 2*c ))
 	done	
 	n=$(( 2*n ))
 done
 
-echo "!!!!!!!iteration number!!!!!!!!"
+echo "Running experiment for variable number of iterations..."
 itr=100
-m=1
+c=1
 while (( $itr <= 1000 ))
 do	
-	echo "iterations: $itr"
-	while (($m <= 2))
+	echo "Iterations: $itr"
+	while (( $m <= 2 ))
 	do
-		echo "number of cores: $m"
+		echo "Number of cores: $c"
 		for i in `seq 1 5`
 		do
-		echo "measurement: $i"
-		if ((m==1))
-		then
-			./relax-seq.o 2048 $itr
-			./relax-seq-vec.o 2048 $itr
-		fi
-		./relax-omp.o 2048 $itr $m
-		./relax-omp-vec.o 2048 $itr $m
-		mpirun -N $m relax-mpi.o 2048 $itr
-		mpirun -N $m relax-mpi-vec.o 2048 $itr 
-		
+            echo "Measurement: $i"
+            if (( c==1 ))
+            then
+                ./relax-seq.o 2048 $itr
+                ./relax-seq-vec.o 2048 $itr
+            fi
+            ./relax-omp.o 2048 $itr $c
+            ./relax-omp-vec.o 2048 $itr $c
+            mpirun -n $c relax-mpi.o 2048 $itr
+            mpirun -n $c relax-mpi-vec.o 2048 $itr 
 		done
-		m=$((2*m))
+		m=$(( 2*c ))
 	done
 	itr=$(( 100+itr ))	
 done
